01907336eeec73232c5da7557e6d225890fe068f
kumol <kumol01763@gmail.com>
2020-09-20 23:49:10 +0600

    first route

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-
 let config = require('./config/config.json');
 require('./models/db');
 const express = require("express");
 const app = express();
 var bodyParser = require('body-parser');
+let userRouter = require("./api/user");
 app.listen(process.env.PORT|| config.development.PORT,(err)=>{
     if(!err){
         console.log(`app is running on port ${config.development.PORT}`);
@@ -11,7 +11,7 @@ app.listen(process.env.PORT|| config.development.PORT,(err)=>{
         console.log("Return error");
     }
 });
-
+app.use("/user",userRouter);
 app.use("/api",(res)=>{
     res.statusCode(200);
 })
