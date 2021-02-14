3cf3d5c72782af020b378605ce20e2aba322ac74
kumol <kumol01763@gmail.com>
2021-02-15 03:32:17 +0600

    error fixed while getting organization

--- api/controller/organizationController.js
+++ api/controller/organizationController.js
@@ -5,7 +5,7 @@ module.exports.getOrganizationById = async (req,res,next) =>{
         console.log(req.body.id);
         const org = await Organization.findOne({_id:req.params.id});
         console.log(org);
-        res.status(200).json({message:"success",organization:org.result[0]});
+        res.status(200).json({message:"success",organization:org});
     }catch(error){
         res.status(500).json({message:"internal server error"});
     }
